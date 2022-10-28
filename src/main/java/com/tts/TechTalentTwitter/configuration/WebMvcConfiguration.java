62afd9341b167eecbd0b12c94fbb973dd94ead0a
mosesrc <ryanchristophermoses@gmail.com>
2022-10-27 20:14:43 -0400

    Final Assignment Commit

---
+++ src/main/java/com/tts/TechTalentTwitter/configuration/WebMvcConfiguration.java
@@ -0,0 +1,13 @@
+package com.tts.TechTalentTwitter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfiguration implements WebMvcConfigurer {
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder;
+	}
+}
---
+++ src/test/java/com/tts/TechTalentTwitter/controller/UserControllerTest.java
@@ -0,0 +1,23 @@
+package com.tts.TechTalentTwitter.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.tts.TechTalentTwitter.model.User;
+
+public class UserControllerTest {
+
+	@Test
+	public void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUser() {
+		User user = new User();
+		
+		assertEquals(user, new User());
+	}
+
+}
